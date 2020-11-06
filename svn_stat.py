174db2a4a77fe11b5c142db656e065a1fb458279
Kiwan Kang <kwkang@nm-gurobal.com>
2020-11-06 19:21:26 +0900

    Update svn_stat.py

--- svn_stat.py
+++ svn_stat.py
@@ -5,9 +5,36 @@ import argparse
 import os
 import subprocess
 from tqdm import tqdm
+import mysql.connector
+from datetime import datetime
 
-def read_config(file):
-    sys.exit()
+def insert_to_db(host, user, passwd, database, dict):
+    mydb = mysql.connector.connect(host=host, user=user, password=passwd, database=database)
+    cur = mydb.cursor()
+    now = datetime.now()
+    cur.execute('CREATE DATABASE IF NOT EXISTS `SVN`')
+
+    tbl_name = f'SVN_STAT_{now.strftime("%y%m%d_%H%M%S")}'
+    cur.execute(f"""
+        CREATE TABLE IF NOT EXISTS `SVN`.`{tbl_name}` (
+            name VARCHAR(128) NOT NULL PRIMARY KEY, 
+            commit_count INT DEFAULT 0, 
+            update_files INT DEFAULT 0, 
+            update_lines INT DEFAULT 0
+        )""")
+    query = f'INSERT INTO `SVN`.`{tbl_name}` VALUES '
+
+    #print(dict)
+    comma = True
+    for key, val in dict.items():
+        if comma == False:
+            query += ","
+        query += f"('{key}', {val['commit_count']}, {val['update_files']}, {val['update_lines']})"
+        comma = False
+    #print(query)
+    cur.execute(query)
+    mydb.commit()
+
 
 def main():
     parser = argparse.ArgumentParser(description='Get subversion statistics.')
@@ -63,9 +90,9 @@ def main():
             for line in dec.split("\n"):
                 if len(line) < 2:
                     continue
-                if line[0] == '+' and line[1] == ' ':
+                if line[0] == '+' and line[1] != '+':
                     plus += 1
-                elif line[0] == '-' and line[1] == ' ':
+                elif line[0] == '-' and line[1] != '-':
                     minus += 1
 
             files = 0
@@ -88,7 +115,8 @@ def main():
         pbar.close()
 
         print(f'Total: {count:d}')
-        print(dict)
+
+        insert_to_db(json_data['mysql_host'], json_data['mysql_user'], json_data['mysql_pass'], json_data['mysql_database'], dict)
 
 
 if __name__ == '__main__':
